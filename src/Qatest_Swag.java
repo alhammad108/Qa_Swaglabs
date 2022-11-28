d78171951a5075446952a5913e2ff0d835833b4e
alhammad108 <115951421+alhammad108@users.noreply.github.com>
2022-11-28 20:47:20 +0300

    Update Qatest_Swag.java

--- src/Qatest_Swag.java
+++ src/Qatest_Swag.java
@@ -19,12 +19,12 @@ driver.manage().window().maximize();
 
 String User_name="standard_user";
 
-String User_Password="secret_sauce";
+String User_password="secret_sauce";
 
 
 driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys(User_name);
 Thread.sleep(2000);
-driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys(User_Password);
+driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys(User_password);
 Thread.sleep(2000);
 driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
 
@@ -34,12 +34,12 @@ List<WebElement> Listadd =driver.findElements(By.className("btn"));
 for(int i=0;i<Listadd.size();i++) {
 Listadd.get(i).click();
 }
-int expectedtittle=6;
-String acctualtittle=driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a/span")).getText();
+int expectedTittle=6;
+String acctualTittle=driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a/span")).getText();
 
 Thread.sleep(2000);
-Assert.assertEquals(acctualtittle, expectedtittle);
-System.out.println(acctualtittle);
+Assert.assertEquals(acctualTittle, expectedTittle);
+System.out.println(acctualTittle);
 
 	}
 
